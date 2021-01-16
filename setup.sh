afd2e4bd9daa9ba09700a2d9988587c840513c3b
Joel Ermantraut <joelermantraut@gmail.com>
2021-01-16 01:55:07 -0300

    Finishing Testing

--- setup.sh
+++ setup.sh
@@ -5,16 +5,6 @@ echo
 export HOME=/home/joel
 # Declare variables
 
-cd /etc/sudoers.d/
-sudo touch "$(id -un)" 
-echo "Defaults timestamp_timeout=-1" >> "$(id -un)"
-# Disable sudo password timeout
-
-echo
-echo "Configuring MAKEPKG to use all 8 cores"
-sudo sed -i -e 's|[#]*MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|g' makepkg.conf
-sudo sed -i -e 's|[#]*COMPRESSXZ=.*|COMPRESSXZ=(xz -c -T 8 -z -)|g' makepkg.conf
-
 #./xorg.sh
 #./audio.sh
 #./desktop.sh
@@ -27,12 +17,19 @@ echo "Installing Other Programs"
 #bash -c "$(wget -q -O - https://linux.kite.com/dls/linux/current)" # Kite Installer
 
 sudo git clone "https://github.com/JoelErmantraut1/dotfiles.git"
+cd dotfiles
 sudo rm -r .git
 sudo rm .gitignore
 sudo rm README.md
-yes | sudo cp -r /dotfiles/{,.[^.]}* $HOME
+cd ..
+yes | sudo cp -r dotfiles/. $HOME
 
 #sudo systemctl enable lightdm
+
+echo
+echo "Configuring MAKEPKG to use all 8 cores"
+sudo sed -i -e 's|[#]*MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|g' makepkg.conf
+sudo sed -i -e 's|[#]*COMPRESSXZ=.*|COMPRESSXZ=(xz -c -T 8 -z -)|g' makepkg.conf
 
 echo "All ready"
 echo
