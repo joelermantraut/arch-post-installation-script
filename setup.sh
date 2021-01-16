c61f378bb00dc6ddd89a8ea5e4d980ec540b97b7
Joel Ermantraut <joelermantraut@gmail.com>
2021-01-16 01:24:25 -0300

    Testing configuration

--- setup.sh
+++ setup.sh
@@ -3,21 +3,36 @@ echo "Running Setup"
 echo
 
 export HOME=/home/joel
+# Declare variables
 
-./xorg.sh
-./audio.sh
-./desktop.sh
-./software-pacman.sh
-./software-aur.sh
-./software-git.sh
+cd /etc/sudoers.d/
+sudo touch "$(id -un)" 
+echo "Defaults timestamp_timeout=-1" >> "$(id -un)"
+# Disable sudo password timeout
+
+echo
+echo "Configuring MAKEPKG to use all 8 cores"
+sudo sed -i -e 's|[#]*MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|g' makepkg.conf
+sudo sed -i -e 's|[#]*COMPRESSXZ=.*|COMPRESSXZ=(xz -c -T 8 -z -)|g' makepkg.conf
+
+#./xorg.sh
+#./audio.sh
+#./desktop.sh
+#./software-pacman.sh
+#./software-aur.sh
+#./software-git.sh
+#./software-python.sh
 
 echo "Installing Other Programs"
-bash -c "$(wget -q -O - https://linux.kite.com/dls/linux/current)" # Kite Installer
+#bash -c "$(wget -q -O - https://linux.kite.com/dls/linux/current)" # Kite Installer
 
 sudo git clone "https://github.com/JoelErmantraut1/dotfiles.git"
-sudo mv -f /dotfiles/{,.[^.]}* $HOME
+sudo rm -r .git
+sudo rm .gitignore
+sudo rm README.md
+yes | sudo cp -r /dotfiles/{,.[^.]}* $HOME
 
-sudo systemctl enable lightdm
+#sudo systemctl enable lightdm
 
 echo "All ready"
 echo
